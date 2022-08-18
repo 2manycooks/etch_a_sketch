5e105ade7470129bdcfc2428003b488b5c99c2d9
2manycooks <73556731+2manycooks@users.noreply.github.com>
2022-08-18 09:57:29 -0700

    cant figure out removeeventlistener

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // The magic happens here!
+
+let screen = document.querySelector('#screen')
+let pixel = document.querySelector('.pixel')
+// important to use getElementsByClassName because HTML collections, unlike NodeLists, are live collections, and thus update when lines 13-20 run.
+let allPixels = document.getElementsByClassName('.pixel')
+
+// buttons
+let clearButton = document.querySelector('#clear-screen-button')
+let startButton = document.querySelector('#start-button')
+let stopButton = document.querySelector('#stop-button')
+
+
+pixel.addEventListener('mouseenter', (e) => {
+    pixel.classList.add('colored-in')
+})
+
+for (let i =0; i < 7423; i++) {
+    let newPixel = document.createElement('div')
+    newPixel.classList.add('pixel')
+    // newPixel.addEventListener('mouseenter', function(e) {
+    //     e.target.classList.add('colored-in')
+    // })
+    screen.append(newPixel)
+}
+
+clearButton.addEventListener("click", (e) => {
+    // way #2 is doing it here with querySelector after they have all been added
+    let everyPixel = document.querySelectorAll('.pixel')
+    everyPixel.forEach(pixel => {
+        pixel.classList.remove('colored-in')
+    })
+})
+
+let addDrawingCapability = () => {
+    let everyPixel = document.querySelectorAll('.pixel')
+    everyPixel.forEach(pixel => {
+        pixel.addEventListener('mouseenter', () => {
+            pixel.classList.add('colored-in')
+        })
+    })
+}
+
+let startDrawing = () => {
+    addDrawingCapability()
+}
+
+let stopDrawing = () => {
+    let everyPixel = document.querySelectorAll('.pixel')
+    everyPixel.forEach(pixel => {
+        pixel.removeEventListener('mouseenter', addDrawingCapability)
+    })
+}
+
+startButton.addEventListener("click", startDrawing)
+
+stopButton.addEventListener("click", stopDrawing)
